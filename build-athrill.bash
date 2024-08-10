2ad35e5fb6a22ea9931b65b54ef0a49514e3276e
Takashi Mori <kanetugu2015@gmail.com>
2024-08-10 09:14:17 +0900

    add build options

--- build-athrill.bash
+++ build-athrill.bash
@@ -18,7 +18,8 @@ else
 fi
 cd athrill-target-v850e2m/cmake-build
 cmake ..
-make
+
+make timer32=true serial_fifo_enable=true vdev_disable=true enable_bt_serial=true skip_clock_bugfix=true supress_detect_error=true etrobo_optimize=true reuse_port=true
 
 ./athrill2
 if [ $? -eq 0 ]
